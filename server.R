918dff51f00187e218b8556886f413705526f77c
Rico Derks <r.j.e.derks@lumc.nl>
2019-03-12 09:54:45 +0100

    new pc

--- server.R
+++ server.R
@@ -487,36 +487,37 @@ shinyServer(
     output$hover_info <- renderUI({
       req(input$plot_hover)
       req(plot_df())
-      
+
       # if for the samples the bar graph is selected hover info should not be available
       # there are some problems getting the correct info, so for now leave out
       if (input$select_sample_graph == "bar") {
         return(NULL)
       }
-      
+
       hover <- input$plot_hover
       point <- nearPoints(df = plot_df(),
                           coordinfo = hover,
                           threshold = 5,
-                          maxpoints = 1, addDist = TRUE)
+                          maxpoints = NULL,
+                          addDist = TRUE)
       # if there is no point return nothing
-      if (nrow(point) == 0) return(NULL)
-      
+      #if (nrow(point) == 0) return(NULL)
+
       # calculate point position INSIDE the image as percent of total dimensions
       # from left (horizontal) and from top (vertical)
       left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
       top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
-      
+
       # calculate distance from left and bottom side of the picture in pixels
       left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
       top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
-      
+
       # create style property fot tooltip
       # background color is set so tooltip is a bit transparent
       # z-index is set so we are sure are tooltip will be on top
       style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
                       "left:", left_px + 2, "px; top:", top_px + 2, "px;")
-      
+
       # actual tooltip created as wellPanel
       wellPanel(
         style = style,
